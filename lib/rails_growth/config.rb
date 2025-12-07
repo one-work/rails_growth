b9b20c68afe1e7041b9c70f9202787ea63b4979f
qinmingyuan <mingyuan0715@foxmail.com>
2025-12-07 19:31:20 +0800

    auto commit

--- lib/rails_growth/config.rb
+++ lib/rails_growth/config.rb
@@ -1,13 +1,9 @@
-require 'active_support/configurable'
-
 module RailsGrowth
-  include ActiveSupport::Configurable
+  mattr_accessor :config, default: ActiveSupport::OrderedOptions.new
 
-  configure do |config|
-    config.rate_to_reward = 0.3
-    config.rate_to_royalty = 0.5
-    config.default_max_piece = 10
-    config.default_min_piece = 1
-  end
+  config.rate_to_reward = 0.3
+  config.rate_to_royalty = 0.5
+  config.default_max_piece = 10
+  config.default_min_piece = 1
 
 end
