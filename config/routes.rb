72c5644c13dacfd77f3f6bc11b292dbbc6a41c01
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-21 12:39:21 +0800

    auto commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+Rails.app.routes.draw do
   namespace 'growth', defaults: { business: 'growth' } do
     namespace :admin, defaults: { namespace: 'admin' } do
       resources :aims do
